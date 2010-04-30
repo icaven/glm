a68bb0c534f52287f2c0802486f1f29b5c2e1459
Christophe Riccio <christophe.riccio@gmail.com>
2010-04-30 11:11:09 +0100

    Clean up promoted extensions

--- glm/ext.hpp
+++ glm/ext.hpp
@@ -2,7 +2,7 @@
 // OpenGL Mathematics Copyright (c) 2005 - 2010 G-Truc Creation (www.g-truc.net)
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Created : 2009-05-01
-// Updated : 2010-02-20
+// Updated : 2010-04-30
 // Licence : This source is under MIT License
 // File    : glm/ext.hpp
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -10,16 +10,16 @@
 #ifndef glm_ext
 #define glm_ext
 
-#include "gtc/double_float.hpp"
-#include "gtc/half_float.hpp"
-#include "gtc/matrix_access.hpp"
-#include "gtc/matrix_operation.hpp"
-#include "gtc/matrix_projection.hpp"
-#include "gtc/matrix_transform.hpp"
-#include "gtc/quaternion.hpp"
-#include "gtc/swizzle.hpp"
-#include "gtc/type_precision.hpp"
-#include "gtc/type_ptr.hpp"
+#include "./gtc/double_float.hpp"
+#include "./gtc/half_float.hpp"
+#include "./gtc/matrix_access.hpp"
+#include "./gtc/matrix_operation.hpp"
+#include "./gtc/matrix_projection.hpp"
+#include "./gtc/matrix_transform.hpp"
+#include "./gtc/quaternion.hpp"
+#include "./gtc/swizzle.hpp"
+#include "./gtc/type_precision.hpp"
+#include "./gtc/type_ptr.hpp"
 
 #include "./gtx/associated_min_max.hpp"
 #include "./gtx/bit.hpp"
@@ -31,7 +31,6 @@
 #include "./gtx/compatibility.hpp"
 #include "./gtx/component_wise.hpp"
 #include "./gtx/determinant.hpp"
-#include "./gtx/double_float.hpp"
 #include "./gtx/epsilon.hpp"
 #include "./gtx/euler_angles.hpp"
 #include "./gtx/extend.hpp"
@@ -40,7 +39,6 @@
 #include "./gtx/fast_square_root.hpp"
 #include "./gtx/fast_trigonometry.hpp"
 #include "./gtx/gradient_paint.hpp"
-#include "./gtx/half_float.hpp"
 #include "./gtx/handed_coordinate_space.hpp"
 #include "./gtx/inertia.hpp"
 #include "./gtx/integer.hpp"
@@ -55,7 +53,6 @@
 #include "./gtx/matrix_projection.hpp"
 #include "./gtx/matrix_query.hpp"
 #include "./gtx/matrix_selection.hpp"
-//#include "./gtx/matx.hpp"
 #include "./gtx/mixed_product.hpp"
 #include "./gtx/norm.hpp"
 #include "./gtx/normal.hpp"
@@ -82,7 +79,6 @@
 #include "./gtx/vector_access.hpp"
 #include "./gtx/vector_angle.hpp"
 #include "./gtx/vector_query.hpp"
-//#include "./gtx/vecx.hpp"
 #include "./gtx/verbose_operator.hpp"
 
 #include "./img/multiple.hpp"
