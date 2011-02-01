9bad7f048e1e622763e72629efd9437bca7c00c1
Christophe Riccio <g.truc.creation@gmail.com>
2011-02-01 10:53:18 +0000

    Force SSE AVX instructions

--- glm/setup.hpp
+++ glm/setup.hpp
@@ -249,7 +249,7 @@
 /////////////////
 // Platform 
 
-// User defines: GLM_FORCE_PURE
+// User defines: GLM_FORCE_PURE GLM_FORCE_SSE2 GLM_FORCE_AVX
 
 #define GLM_ARCH_PURE		0x0000 //(0x0000)
 #define GLM_ARCH_SSE2		0x0001 //(0x0001)
@@ -258,6 +258,12 @@
 
 #if(defined(GLM_FORCE_PURE))
 #	define GLM_ARCH GLM_ARCH_PURE
+#elif(defined(GLM_FORCE_AVX))
+#	define GLM_ARCH GLM_ARCH_AVX
+#elif(defined(GLM_FORCE_SSE3))
+#	define GLM_ARCH GLM_ARCH_SSE3
+#elif(defined(GLM_FORCE_SSE2))
+#	define GLM_ARCH GLM_ARCH_SSE2
 #elif((GLM_COMPILER & GLM_COMPILER_VC) && (defined(_M_IX86) || defined(_M_X64)))
 #	if(defined(_M_CEE_PURE))
 #		define GLM_ARCH GLM_ARCH_PURE
