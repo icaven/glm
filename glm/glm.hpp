7cf85dddb02704f7430cf115b28a1a17dbf4cdbf
Christophe Riccio <g.truc.creation@gmail.com>
2011-02-09 13:55:55 +0000

    Fixed typo

--- glm/glm.hpp
+++ glm/glm.hpp
@@ -45,7 +45,7 @@ namespace glm
 		bool main_core();
 	}//namespace test
 
-	//! GLM core. Namespace that includes all the feature define by GLSL 1.30.8 specification. This namespace is included in glm namespace.
+	//! GLM core. Namespace that includes all the feature define by GLSL 4.10.6 specification. This namespace is included in glm namespace.
 	namespace core
 	{
 		//! Scalar, vectors and matrices 
