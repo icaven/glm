41c8cc204d09a05ac34027ce65e2cf72e1b177dc
Christophe Riccio <g.truc.creation@gmail.com>
2011-02-12 22:56:18 +0000

    Updated version

--- glm/setup.hpp
+++ glm/setup.hpp
@@ -17,7 +17,7 @@
 #define GLM_VERSION_MAJOR			0
 #define GLM_VERSION_MINOR			9
 #define GLM_VERSION_PATCH			0
-#define GLM_VERSION_REVISION		7
+#define GLM_VERSION_REVISION		8
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Common values
