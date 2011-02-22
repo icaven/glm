787a60956f1980f8dce9e28ea47a4651073cf42c
Christophe Riccio <g.truc.creation@gmail.com>
2011-02-22 01:35:55 +0000

    Added files for GLM_GTX_ulp extension

---
+++ glm/gtx/ulp.hpp
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// OpenGL Mathematics Copyright (c) 2005 - 2011 G-Truc Creation (www.g-truc.net)
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Created : 2011-02-21
+// Updated : 2009-02-21
+// Licence : This source is under MIT License
+// File    : glm/gtx/ulp.hpp
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Dependency:
+// - GLM core
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef glm_gtx_ulp
+#define glm_gtx_ulp
+
+// Dependency:
+#include "../glm.hpp"
+
+#if(defined(GLM_MESSAGES) && !defined(glm_ext))
+#	pragma message("GLM: GLM_GTX_ulp extension included")
+#endif
+
+namespace glm
+{
+	namespace gtx{
+	//! GLM_GTX_ulp extension: Precision calculation functions
+	namespace spline
+	{
+		/// \addtogroup gtx_ulp
+		///@{
+
+		//! Return the number of ULP between 2 variables.
+		//! From GLM_GTX_ulp extension.
+		template <typename genType>
+		genIType ulp(
+			genType const & a,
+			genType const & b);
+
+		///@}
+
+	}//namespace ulp
+	}//namespace gtx
+}//namespace glm
+
+#include "spline.inl"
+
+namespace glm{using namespace gtx::spline;}
+
+#endif//glm_gtx_spline
+
