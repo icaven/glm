3ef449763504f790c6313e3b1804660446de3414
Christophe Riccio <g.truc.creation@gmail.com>
2011-04-09 11:01:42 +0100

    Updated test

--- test/gtc/gtc_type_ptr.cpp
+++ test/gtc/gtc_type_ptr.cpp
@@ -11,9 +11,17 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+bool test_make_pointer()
+{
+    float ArrayA[] = {1, 2, 3, 4};
+    glm::vec4 Vec4A = glm::make_vec4(ArrayA);
+}
+
 int main()
 {
 	int Failed = 0;
+    
+    Failed += test_make_pointer() ? 0 : 1;
 
 	return Failed;
 }
