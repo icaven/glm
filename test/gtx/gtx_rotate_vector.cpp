d71d20a6ef566c947e8035c3ff9da452723e0ec0
Christophe Riccio <g.truc.creation@gmail.com>
2011-05-16 09:52:53 +0100

    Added test basic structure

---
+++ test/gtx/gtx_rotate_vector.cpp
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// OpenGL Mathematics Copyright (c) 2005 - 2011 G-Truc Creation (www.g-truc.net)
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Created : 2011-05-16
+// Updated : 2011-05-16
+// Licence : This source is under MIT licence
+// File    : test/gtx/rotate_vector.cpp
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <glm/glm.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+
+int test_()
+{
+    int Error = 0;
+    
+  
+    return Error;
+}
+
+int main()
+{
+	int Error = 0;
+    Error += test_();
+    
+	return Error;
+}
+
+
