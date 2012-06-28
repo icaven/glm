a979cb483eb0624ec86e88f230ee85a70a3dfb54
Christophe Riccio <mail@g-truc.net>
2012-06-28 19:15:57 +0100

    Updated readme for GLM 0.9.3.4 release

--- glm/core/type_vec3.hpp
+++ glm/core/type_vec3.hpp
@@ -84,13 +84,12 @@ namespace detail
 		union {value_type x, r, s;};
 		union {value_type y, g, t;};
 		union {value_type z, b, p;};
-/*
+
 #		if(defined(GLM_SWIZZLE))
 			// Defines all he swizzle operator as functions
 			GLM_SWIZZLE_GEN_REF_FROM_VEC3(T, detail::tvec3, detail::tref2, detail::tref3)
 			GLM_SWIZZLE_GEN_VEC_FROM_VEC3(T, detail::tvec3, detail::tvec2, detail::tvec3, detail::tvec4)
 #		endif//(defined(GLM_SWIZZLE))
-*/
 #	else //(GLM_COMPONENT == GLM_COMPONENT_ONLY_XYZW)
 		value_type x, y, z;
 
