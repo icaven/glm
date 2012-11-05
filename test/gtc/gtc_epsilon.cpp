cdf5d19275af1f0346864672c475491ef1ac613d
Christophe Riccio <mail@g-truc.net>
2012-11-06 00:44:48 +0100

    Fixed space characters, using tab

--- test/gtc/gtc_epsilon.cpp
+++ test/gtc/gtc_epsilon.cpp
@@ -17,36 +17,36 @@ int test_equal()
 {
 	int Error(0);
 	
-    {
-        T A = glm::epsilon<T>();
-        T B = glm::epsilon<T>();
-        Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
-    }
-  
-    {
-        T A(0);
-        T B = T(0) + glm::epsilon<T>();
-        Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
-    }
- 
-    {
-        T A(0);
-        T B = T(0) - glm::epsilon<T>();
-        Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
-    }
-   
-    {
-        T A = T(0) + glm::epsilon<T>();
-        T B = T(0);
-        Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
-    }
-    
-    {
-        T A = T(0) - glm::epsilon<T>();
-        T B = T(0);
-        Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
-    }
-    
+	{
+		T A = glm::epsilon<T>();
+		T B = glm::epsilon<T>();
+		Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
+	}
+
+	{
+		T A(0);
+		T B = T(0) + glm::epsilon<T>();
+		Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
+	}
+
+	{
+		T A(0);
+		T B = T(0) - glm::epsilon<T>();
+		Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
+	}
+
+	{
+		T A = T(0) + glm::epsilon<T>();
+		T B = T(0);
+		Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
+	}
+
+	{
+		T A = T(0) - glm::epsilon<T>();
+		T B = T(0);
+		Error += glm::epsilonEqual(A, B, glm::epsilon<T>()) ? 0 : 1;
+	}
+
 	return Error;
 }
 
@@ -54,9 +54,9 @@ int main()
 {
 	int Error(0);
 
-    Error += test_equal<glm::half>();
-    Error += test_equal<float>();
-    Error += test_equal<double>();
+	Error += test_equal<glm::half>();
+	Error += test_equal<float>();
+	Error += test_equal<double>();
 
 	return Error;
 }
