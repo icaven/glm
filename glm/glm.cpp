3c7989486edfba1890f869dd1fcec927ddc426e0
Christophe Riccio <mail@g-truc.net>
2013-04-23 16:39:15 +0200

    Fixed literals in dual quaternion implementation

--- glm/glm.cpp
+++ glm/glm.cpp
@@ -28,6 +28,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/dual_quaternion.hpp>
 
 namespace glm{
 namespace detail
@@ -513,6 +514,32 @@ template struct tmat4x4<int64, highp>;
 template struct tmat4x4<float16, highp>;
 template struct tmat4x4<float32, highp>;
 template struct tmat4x4<float64, highp>;
+
+// tquat type explicit instantiation
+template struct tquat<float16, lowp>;
+template struct tquat<float32, lowp>;
+template struct tquat<float64, lowp>;
+
+template struct tquat<float16, mediump>;
+template struct tquat<float32, mediump>;
+template struct tquat<float64, mediump>;
+
+template struct tquat<float16, highp>;
+template struct tquat<float32, highp>;
+template struct tquat<float64, highp>;
+
+//tdualquat type explicit instantiation
+template struct tdualquat<float16, lowp>;
+template struct tdualquat<float32, lowp>;
+template struct tdualquat<float64, lowp>;
+
+template struct tdualquat<float16, mediump>;
+template struct tdualquat<float32, mediump>;
+template struct tdualquat<float64, mediump>;
+
+template struct tdualquat<float16, highp>;
+template struct tdualquat<float32, highp>;
+template struct tdualquat<float64, highp>;
 
 }//namespace detail
 }//namespace glm
