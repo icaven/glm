c23cb8cd593b5d7b5ab859b218f83080a3182951
Christophe Riccio <mail@g-truc.net>
2014-10-20 21:34:56 +0200

    Added GLM_NEVER_INLINE

--- glm/detail/setup.hpp
+++ glm/detail/setup.hpp
@@ -687,15 +687,20 @@
 #if defined(GLM_FORCE_INLINE)
 #	if GLM_COMPILER & GLM_COMPILER_VC
 #		define GLM_INLINE __forceinline
+#		define GLM_NEVER_INLINE __declspec((noinline))
 #	elif GLM_COMPILER & GLM_COMPILER_GCC
-#		define GLM_INLINE __attribute__((always_inline)) inline
+#		define GLM_INLINE inline __attribute__((__always_inline__))
+#		define GLM_NEVER_INLINE __attribute__((__noinline__))
 #	elif GLM_COMPILER & GLM_COMPILER_CLANG
-#		define GLM_INLINE __attribute__((always_inline))
+#		define GLM_INLINE __attribute__((__always_inline__))
+#		define GLM_NEVER_INLINE __attribute__((__noinline__))
 #	else
 #		define GLM_INLINE inline
+#		define GLM_NEVER_INLINE
 #	endif//GLM_COMPILER
 #else
 #	define GLM_INLINE inline
+#	define GLM_NEVER_INLINE
 #endif//defined(GLM_FORCE_INLINE)
 
 #define GLM_FUNC_DECL GLM_CUDA_FUNC_DECL
