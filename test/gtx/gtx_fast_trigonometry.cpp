43387c81de38e3cdcafa703b53191b9d1a8b77d5
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2014-11-05 15:05:33 +0100

    test perf fastAcos

--- test/gtx/gtx_fast_trigonometry.cpp
+++ test/gtx/gtx_fast_trigonometry.cpp
@@ -75,6 +75,27 @@ namespace fastTan{
 	}
 }
 
+namespace fastAcos{
+	int perf()
+	{
+		const float begin = -glm::pi<float>();
+		const float end = glm::pi<float>();
+		float result = 0.f;
+		const std::clock_t timestamp1 = std::clock();
+		for (float i = begin; i<end; i = nextafterf(i, end))
+			result = glm::fastAcos(i);
+		const std::clock_t timestamp2 = std::clock();
+		for (float i = begin; i<end; i = nextafterf(i, end))
+			result = glm::acos(i);
+		const std::clock_t timestamp3 = std::clock();
+		const std::clock_t time_fast = timestamp2 - timestamp1;
+		const std::clock_t time_default = timestamp3 - timestamp2;
+		std::printf("fastAcos Time %d clocks\n", static_cast<unsigned int>(time_fast));
+		std::printf("acos Time %d clocks\n", static_cast<unsigned int>(time_default));
+		return time_fast < time_default ? 0 : 1;
+	}
+}
+
 
 int main()
 {
@@ -83,6 +104,7 @@ int main()
 	Error += ::fastCos::perf();
 	Error += ::fastSin::perf();
 	Error += ::fastTan::perf();
+	Error += ::fastAcos::perf();
 
 	return Error;
 }
