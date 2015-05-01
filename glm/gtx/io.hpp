e5901113750895dcf67d8075f484fbfbac349b1d
jan p springer <jsd@igroup.org>
2015-05-01 13:42:56 +0100

    added: 'operator<<(std::pair<tmat4x4<T,P>, tmat4x4<T,P>>)' routed to 'operator<<(std::pair<tmat4x4<T,P> const, tmat4x4<T,P> const>)' to avoid specifier removal by 'std::make_pair<>' and follow-up type-deduction problems; see [http://stackoverflow.com/questions/29985791/]

--- glm/gtx/io.hpp
+++ glm/gtx/io.hpp
@@ -214,12 +214,18 @@ namespace glm
 	template <typename CTy, typename CTr, typename T, precision P>
 	GLM_FUNC_DECL std::basic_ostream<CTy,CTr>& operator<<(std::basic_ostream<CTy,CTr>&, tmat4x4<T,P> const&);
 
-	template <typename CTy, typename CTr, typename T, precision P>
+  template <typename CTy, typename CTr, typename T, precision P>
 	GLM_FUNC_DECL std::basic_ostream<CTy,CTr> & operator<<(
 		std::basic_ostream<CTy,CTr> &,
 		std::pair<tmat4x4<T,P> const,
 		tmat4x4<T,P> const> const &);
 
+  template <typename CTy, typename CTr, typename T, precision P>
+	GLM_FUNC_DECL std::basic_ostream<CTy,CTr> & operator<<(
+		std::basic_ostream<CTy,CTr> &,
+		std::pair<tmat4x4<T,P>,
+		tmat4x4<T,P>> const &);
+  
 	/// @}
 }//namespace glm
 
