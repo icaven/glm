3400352e8140b4abc9fef756d171125f74c85072
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2015-07-25 14:20:04 -0400

    Reverse the mat::rows and mat::cols fields
    - Oops, I mixed up the row_type and col_type typedefs

--- glm/detail/type_mat2x3.hpp
+++ glm/detail/type_mat2x3.hpp
@@ -52,8 +52,8 @@ namespace glm
 
 #		ifdef GLM_META_PROG_HELPERS
 			static GLM_RELAXED_CONSTEXPR length_t components = 2;
-			static GLM_RELAXED_CONSTEXPR length_t cols = 3;
-			static GLM_RELAXED_CONSTEXPR length_t rows = 2;
+			static GLM_RELAXED_CONSTEXPR length_t cols = 2;
+			static GLM_RELAXED_CONSTEXPR length_t rows = 3;
 			static GLM_RELAXED_CONSTEXPR precision prec = P;
 #		endif//GLM_META_PROG_HELPERS
 
