627d36fd4781774bf4f68651cb95b0934573d90a
Christophe Riccio <christophe.riccio@unity3d.com>
2015-10-22 00:31:15 +0200

    Updated GLM version

--- glm/detail/setup.hpp
+++ glm/detail/setup.hpp
@@ -42,11 +42,11 @@
 #define GLM_VERSION_MAJOR			0
 #define GLM_VERSION_MINOR			9
 #define GLM_VERSION_PATCH			7
-#define GLM_VERSION_REVISION		1
+#define GLM_VERSION_REVISION		2
 
 #if(defined(GLM_MESSAGES) && !defined(GLM_MESSAGE_VERSION_DISPLAYED))
 #	define GLM_MESSAGE_VERSION_DISPLAYED
-#	pragma message ("GLM: version 0.9.7.1")
+#	pragma message ("GLM: version 0.9.7.2")
 #endif//GLM_MESSAGE
 
 ///////////////////////////////////////////////////////////////////////////////////
