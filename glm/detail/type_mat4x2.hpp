3ec43e9522083cf2eed2edec3f3fb48df93c2d1c
Sami Kankaristo <sami@indiumgames.fi>
2016-03-12 18:31:43 +0200

    Fix incorrect declaration (now matches definition)

--- glm/detail/type_mat4x2.hpp
+++ glm/detail/type_mat4x2.hpp
@@ -193,13 +193,13 @@ namespace glm
 	GLM_FUNC_DECL typename tmat4x2<T, P>::row_type operator*(typename tmat4x2<T, P>::col_type const & v, tmat4x2<T, P> const & m);
 
 	template <typename T, precision P>
+	GLM_FUNC_DECL tmat2x2<T, P> operator*(tmat4x2<T, P> const & m1, tmat2x4<T, P> const & m2);
+
+	template <typename T, precision P>
 	GLM_FUNC_DECL tmat3x2<T, P> operator*(tmat4x2<T, P> const & m1, tmat3x4<T, P> const & m2);
 
 	template <typename T, precision P>
 	GLM_FUNC_DECL tmat4x2<T, P> operator*(tmat4x2<T, P> const & m1, tmat4x4<T, P> const & m2);
-
-	template <typename T, precision P>
-	GLM_FUNC_DECL tmat2x3<T, P> operator*(tmat4x3<T, P> const & m1, tmat2x4<T, P> const & m2);
 
 	template <typename T, precision P>
 	GLM_FUNC_DECL tmat4x2<T, P> operator/(tmat4x2<T, P> const & m, T const & s);
