af4499f3cad3a724282270a0ff403ebcc382105e
Giuseppe Barbieri <elect86@gmail.com>
2017-02-09 17:53:35 +0100

    Update rotate_vector.hpp

--- glm/gtx/rotate_vector.hpp
+++ glm/gtx/rotate_vector.hpp
@@ -94,14 +94,14 @@ namespace glm
 		vec<4, T, P> const & v,
 		T const & angle);
 		
-	//! Rotate a four dimensional vector around the X axis.
+	//! Rotate a four dimensional vector around the Y axis.
 	//! From GLM_GTX_rotate_vector extension.
 	template<typename T, precision P>
 	GLM_FUNC_DECL vec<4, T, P> rotateY(
 		vec<4, T, P> const & v,
 		T const & angle);
 		
-	//! Rotate a four dimensional vector around the X axis.
+	//! Rotate a four dimensional vector around the Z axis.
 	//! From GLM_GTX_rotate_vector extension.
 	template<typename T, precision P>
 	GLM_FUNC_DECL vec<4, T, P> rotateZ(
