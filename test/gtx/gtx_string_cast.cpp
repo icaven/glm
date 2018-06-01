89420dd1f5fdb618be94a7ac46794d6d009c9f00
Vladimír Vondruš <mosra@centrum.cz>
2018-06-01 12:01:25 +0200

    Make the to_string(dualquat) test work under C++03.
    I forgot this is still a thing, sorry.

--- test/gtx/gtx_string_cast.cpp
+++ test/gtx/gtx_string_cast.cpp
@@ -133,7 +133,7 @@ int test_string_cast_dual_quaternion()
 {
 	int Error = 0;
 
-	glm::dualquat Q0 = glm::dualquat({1.0f, 2.0f, 3.0f, 4.0f}, {5.0f, 6.0f, 7.0f, 8.0f});
+	glm::dualquat Q0 = glm::dualquat(glm::quat(1.0f, 2.0f, 3.0f, 4.0f), glm::quat(5.0f, 6.0f, 7.0f, 8.0f));
 	std::string S0 = glm::to_string(Q0);
 	Error += S0 != std::string("dualquat((1.000000, {2.000000, 3.000000, 4.000000}), (5.000000, {6.000000, 7.000000, 8.000000}))") ? 1 : 0;
 
