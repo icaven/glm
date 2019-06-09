8fb71ffd070703d1450ccb4543ce0f6803f037ee
rhoarau <romain.hoarau14@gmail.com>
2019-06-09 16:05:09 +0200

    Initial SYCL support:
    The core and some ext functions should work.

--- glm/detail/setup.hpp
+++ glm/detail/setup.hpp
@@ -410,6 +410,7 @@
 #if GLM_COMPILER & GLM_COMPILER_CUDA
 #	define GLM_CUDA_FUNC_DEF __device__ __host__
 #	define GLM_CUDA_FUNC_DECL __device__ __host__
+#error "oops cuda shit"
 #else
 #	define GLM_CUDA_FUNC_DEF
 #	define GLM_CUDA_FUNC_DECL
@@ -528,6 +529,48 @@
 #else
 #	define GLM_EXPLICIT
 #endif
+
+///////////////////////////////////////////////////////////////////////////////////
+// SYCL
+
+#if GLM_COMPILER==GLM_COMPILER_SYCL
+
+#include <CL/sycl.hpp>
+#include <limits>
+
+namespace glm {
+namespace std {
+    // import sycl function into the namespace glm::std to force their usages.
+    // It's important to use the builtin intrinsics (sin, exp, ...)
+    // of sycl instead the std ones.
+    using namespace cl::sycl;
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // Import some "harmless" std's stuffs used by glm into
+    // the new glm::std namespace.
+    template<typename T>
+    using numeric_limits = ::std::numeric_limits<T>;
+
+    using ::std::size_t;
+
+    using ::std::uint8_t;
+    using ::std::uint16_t;
+    using ::std::uint32_t;
+    using ::std::uint64_t;
+
+    using ::std::int8_t;
+    using ::std::int16_t;
+    using ::std::int32_t;
+    using ::std::int64_t;
+
+    using ::std::make_unsigned;
+    ///////////////////////////////////////////////////////////////////////////////
+} //namespace std
+} //namespace glm
+
+#endif
+
+///////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////
 // Length type: all length functions returns a length_t type.
