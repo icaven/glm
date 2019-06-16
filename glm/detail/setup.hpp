2a8822865c4843e4cc040e9500e55ceb69eed45d
rhoarau <romain.hoarau14@gmail.com>
2019-06-16 13:33:02 +0200

    Fix typos and code style (space to tab).

--- glm/detail/setup.hpp
+++ glm/detail/setup.hpp
@@ -539,31 +539,31 @@
 
 namespace glm {
 namespace std {
-    // import sycl function into the namespace glm::std to force their usages.
-    // It's important to use the builtin intrinsics (sin, exp, ...)
-    // of sycl instead the std ones.
-    using namespace cl::sycl;
+	// Import SYCL's functions into the namespace glm::std to force their usages.
+	// It's important to use the math built-in function (sin, exp, ...)
+	// of SYCL instead the std ones.
+	using namespace cl::sycl;
 
-    ///////////////////////////////////////////////////////////////////////////////
-    // Import some "harmless" std's stuffs used by glm into
-    // the new glm::std namespace.
-    template<typename T>
-    using numeric_limits = ::std::numeric_limits<T>;
+	///////////////////////////////////////////////////////////////////////////////
+	// Import some "harmless" std's stuffs used by glm into
+	// the new glm::std namespace.
+	template<typename T>
+	using numeric_limits = ::std::numeric_limits<T>;
 
-    using ::std::size_t;
+	using ::std::size_t;
 
-    using ::std::uint8_t;
-    using ::std::uint16_t;
-    using ::std::uint32_t;
-    using ::std::uint64_t;
+	using ::std::uint8_t;
+	using ::std::uint16_t;
+	using ::std::uint32_t;
+	using ::std::uint64_t;
 
-    using ::std::int8_t;
-    using ::std::int16_t;
-    using ::std::int32_t;
-    using ::std::int64_t;
+	using ::std::int8_t;
+	using ::std::int16_t;
+	using ::std::int32_t;
+	using ::std::int64_t;
 
-    using ::std::make_unsigned;
-    ///////////////////////////////////////////////////////////////////////////////
+	using ::std::make_unsigned;
+	///////////////////////////////////////////////////////////////////////////////
 } //namespace std
 } //namespace glm
 
