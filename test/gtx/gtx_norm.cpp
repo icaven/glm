a78024d2bfcbf2f2b9e7969c4753ae180917f224
Liam Adams <liam1540@googlemail.com>
2019-07-19 18:11:07 +0100

    Add testing of lMaxNorm.

--- test/gtx/gtx_norm.cpp
+++ test/gtx/gtx_norm.cpp
@@ -1,9 +1,28 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/norm.hpp>
 
+int test_lMaxNorm()
+{
+	int Error(0);
+	
+	{
+		float norm = glm::lMaxNorm(glm::vec3(-1, -2, -3));
+		Error += glm::epsilonEqual(norm, 3.f, 0.00001f) ? 0 : 1;
+	}
+
+	{
+		float norm = glm::lMaxNorm(glm::vec3(2, 3, 1));
+		Error += glm::epsilonEqual(norm, 3.f, 0.00001f) ? 0 : 1;
+	}
+
+	return Error;
+}
+
 int main()
 {
 	int Error(0);
+
+	Error += test_lMaxNorm();
 
 	return Error;
 }
