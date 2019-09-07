247beeb5bf36f3a0b86e61ff56c29c14cf805956
Christophe Riccio <christophe.riccio@unity3d.com>
2019-09-07 11:16:05 +0200

    Bring back quaternion to xyzw storage to remain backward compatible #942

--- glm/detail/type_quat.hpp
+++ glm/detail/type_quat.hpp
@@ -47,7 +47,7 @@ namespace glm
 				typename detail::storage<4, T, detail::is_aligned<Q>::value>::type data;
 			};
 #		else
-			T w, x, y, z;
+			T x, y, z, w;
 #		endif
 
 #		if GLM_SILENT_WARNINGS == GLM_ENABLE
