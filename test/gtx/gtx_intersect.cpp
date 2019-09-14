e7d51a38387f1a3ce815be6ef34eb22b09ec53df
Stefan Maass <sma-github@bitplane.de>
2019-09-14 23:38:44 +0200

    bugfix intersection ray/plane, added related tests

--- test/gtx/gtx_intersect.cpp
+++ test/gtx/gtx_intersect.cpp
@@ -3,6 +3,39 @@
 #include <glm/gtc/epsilon.hpp>
 #include <glm/gtx/intersect.hpp>
 
+int test_intersectRayPlane()
+{
+	int Error = 0;
+	glm::vec3 const PlaneOrigin(0, 0, 1);
+	glm::vec3 const PlaneNormal(0, 0, -1);
+	glm::vec3 const RayOrigin(0, 0, 0);
+	glm::vec3 const RayDir(0, 0, 1);
+
+	// check that inversion of the plane normal has no effect
+	{
+		float Distance = 0;
+		bool const Result = glm::intersectRayPlane(RayOrigin, RayDir, PlaneOrigin, PlaneNormal, Distance);
+		Error += glm::abs(Distance - 1.f) <= std::numeric_limits<float>::epsilon() ? 0 : 1;
+		Error += Result ? 0 : 1;
+	}
+	{
+		float Distance = 0;
+		bool const Result = glm::intersectRayPlane(RayOrigin, RayDir, PlaneOrigin, -1.f * PlaneNormal, Distance);
+		Error += glm::abs(Distance - 1.f) <= std::numeric_limits<float>::epsilon() ? 0 : 1;
+		Error += Result ? 0 : 1;
+	}
+
+	// check if plane is before of behind the ray origin
+	{
+		float Distance = 9.9999f; // value should not be changed
+		bool const Result = glm::intersectRayPlane(RayOrigin, RayDir, -1.f * PlaneOrigin, PlaneNormal, Distance);
+		Error += glm::abs(Distance - 9.9999f) <= std::numeric_limits<float>::epsilon() ? 0 : 1;
+		Error += Result ? 1 : 0; // there is no intersection in front of the ray origin, only behind
+	}
+
+	return Error;
+}
+
 int test_intersectRayTriangle()
 {
 	int Error = 0;
@@ -47,6 +80,7 @@ int main()
 {
 	int Error = 0;
 
+	Error += test_intersectRayPlane();
 	Error += test_intersectRayTriangle();
 	Error += test_intersectLineTriangle();
 
