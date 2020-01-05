a68949eba4f01dc96340133e579f4e74439e8b9b
Christophe Riccio <christophe.riccio@unity3d.com>
2020-01-05 21:09:37 +0100

    Fixed force cxx version tests

--- test/core/core_force_cxx03.cpp
+++ test/core/core_force_cxx03.cpp
@@ -1,4 +1,6 @@
-#define GLM_FORCE_CXX03
+#ifndef GLM_FORCE_CXX03
+#	define GLM_FORCE_CXX03
+#endif
 
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
--- test/core/core_force_cxx11.cpp
+++ test/core/core_force_cxx11.cpp
@@ -1,4 +1,6 @@
-#define GLM_FORCE_CXX11
+#ifndef GLM_FORCE_CXX11
+#	define GLM_FORCE_CXX11
+#endif
 
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
