3ee3659adb0dcbe480317151fec3f613c198e0d0
Christophe Riccio <christophe.riccio@unity3d.com>
2020-01-05 20:57:20 +0100

    Added force defines tests

---
+++ test/core/core_force_arch_unknown.cpp
@@ -0,0 +1,12 @@
+#define GLM_FORCE_ARCH_UNKNOWN
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+int main()
+{
+	int Error = 0;
+
+	return Error;
+}
+
---
+++ test/core/core_force_cxx03.cpp
@@ -0,0 +1,12 @@
+#define GLM_FORCE_CXX03
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+int main()
+{
+	int Error = 0;
+
+	return Error;
+}
+
